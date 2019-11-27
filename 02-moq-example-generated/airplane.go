78e7d8467e9dacce9969be48abff9c5204b00948
Mark Smith <mark@azert.co.uk>
2019-11-27 12:01:24 +0000

    adding go:generate above interface for clarity

--- 02-moq-example-generated/airplane.go
+++ 02-moq-example-generated/airplane.go
@@ -3,6 +3,6 @@ package airplanes
 import "context"
 
 // AirplaneStorer will return all airplanes from a data store.
-type AirplaneStorer interface{
+type AirplaneStorer interface {
 	Airplanes(ctx context.Context) ([]string, error)
 }
